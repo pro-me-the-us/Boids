63fec88f2fc60f8499207d2e7a87667d4ca84c40
pro-me-the-us <shubh005ashish@gmail.com>
2025-03-04 13:52:51 +0530

    Initial Commit

---
+++ CMakeFiles/Boids.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Boids"
+  "Boids.pdb"
+  "CMakeFiles/Boids.dir/src/main.cpp.o"
+  "CMakeFiles/Boids.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Boids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<time.h>
+#include<limits.h>
+
+
+using namespace std;
+
+int main(){
+		cout << "BOIDS" << endl;
+}
